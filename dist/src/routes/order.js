adb82d5f28df71186c2d863e41f4e3cf796caa9f
unknown <xarmzon@gmail.com>
2022-04-15 08:37:22 +0000

    update to deploment

--- dist/src/routes/order.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const order_1 = require("@controller/order");
-const auth_1 = require("@middleware/auth");
-const express_1 = require("express");
-const orderRouter = (0, express_1.Router)();
-orderRouter.all("*", auth_1.userRequired);
-orderRouter.route("/").get(order_1.getOrders).post(order_1.createOrder);
-exports.default = orderRouter;
